eb39f8ed7a85a7a45da4d25324e89cceb62ae1db
Gabriel Milanez Cavalheri <gmcavalheri@gmail.com>
2026-07-03 11:43:01 -0300

    feat(home): adicionado a página de pokedex com integração ao PokéAPI
    - Adicionado os arquivos pokedex.html e pokedex.css que compõem a página estática do aplicativo home
    - Integrado o Front-End com o Back-End (Django)
    - Integrado a PokéAPI com toda a página home
    - Realizado testes na seleção de pokémons e redirecionamento para outras URLs

--- home/views.py
+++ home/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+import requests
+from home.tests_pokeapi import return_pokemon_data
 
 # Create your views here.
+
+
+
+def home(request):
+
+    pokemons = return_pokemon_data()
+
+    return render(request, 'pokedex.html', {'pokemons': pokemons})
+
+
+def pokemon_detail(request, pokemon_id):
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}')
+    data = response.json()
+
+    pokemon = {
+        'id': data['id'],
+        'name': data['name'],
+        'sprite_url': data['sprites']['front_default'],
+        'types': [t['type']['name'] for t in data['types']],
+        'height': data['height'],
+        'weight': data['weight'],
+    }
+    return render(request, 'pokemon_detail.html', {'pokemon': pokemon})
